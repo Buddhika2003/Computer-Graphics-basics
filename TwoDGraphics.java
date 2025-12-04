9c0943837cd0a176b131946df53b105012d76956
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-12-04 22:14:08 +0530

    implementing 2Dgraphics

--- TwoDGraphics.java
+++ TwoDGraphics.java
@@ -2,18 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class DDA extends JFrame{
+public class TwoDGraphics extends JFrame{
 	
 	DDA(){
-		setTitle("DDA algorithm");
-		setSize(600,400);
+		setTitle("TwoDGraphics");
+		setSize(600,600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		//addMouseListener(this);
 		setVisible(true);
 	}
 
 	public static void main(String[]args){
-		new DDA();
+		new TwoDGraphics();
 	}
 }
