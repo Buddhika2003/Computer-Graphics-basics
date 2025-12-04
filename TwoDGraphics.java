6950dc5ee7a26d0ca12d93c3f249d11c7bc4f2b2
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-12-04 22:56:18 +0530

    draw ovals in divided rectangles in frame

--- TwoDGraphics.java
+++ TwoDGraphics.java
@@ -6,24 +6,26 @@ public class TwoDGraphics extends JFrame{
 	
 	TwoDGraphics(){
 		setTitle("TwoDGraphics");
-		setSize(600,600);
+		setSize(400,400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 
 	public void paint(Graphics g){
-		/*g.drawLine(100,100,300,300);
-		g.drawLine(100,100,300,100);
-		g.drawLine(100,100,100,300);
-		g.drawLine(100,300,300,300);
-		g.drawLine(300,300,300,100);	*/
+		
+		g.setColor(Color.RED);
+		g.drawOval(250,50,100,100);
+		g.fillOval(50,50,100,100);
 
-		g.setColor(Color.GREEN);
-		g.drawRect(100,100,200,200);
-		g.fillRect(100,100,200,200);
+		g.setColor(Color.BLUE);
+		g.drawOval(50,250,100,100);
+		g.fillOval(250,250,100,100);
+
 		g.setColor(Color.BLACK);
-		g.drawOval(400,400,100,100);
+		g.drawLine(200,0,200,400);
+		g.drawLine(0,200,400,200);
+
 	}
 
 	public static void main(String[]args){
