c090ad4ec4c82a8e959860f3ccf75cedb16a8595
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-12-04 22:18:05 +0530

    draw a simple line

--- TwoDGraphics.java
+++ TwoDGraphics.java
@@ -4,12 +4,16 @@ import java.awt.event.*;
 
 public class TwoDGraphics extends JFrame{
 	
-	DDA(){
+	TwoDGraphics(){
 		setTitle("TwoDGraphics");
 		setSize(600,600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+
+	public void paint(Graphics g){
+		g.drawLine(100,100,300,300);
 	}
 
 	public static void main(String[]args){
