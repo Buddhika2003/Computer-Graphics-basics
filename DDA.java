652d7914595b6f72910d2d74e009d2083fde336c
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-12-05 17:19:16 +0530

    adding mouseListener and events

--- DDA.java
+++ DDA.java
@@ -2,16 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class DDA extends JFrame{
-	
+public class DDA extends JFrame implements MouseListener{
+	int x1,y1,x2,y2;
+	Graphics g;
+
 	DDA(){
 		setTitle("DDA algorithm");
-		setSize(600,400);
+		setSize(600,600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		//addMouseListener(this);
+		addMouseListener(this);
 		setVisible(true);
+
+		g=getGraphics();
 	}
+
+	public void mouseClicked(MouseEvent e){}
+	public void mousePressed(MouseEvent e){}
+	public void mouseReleased(MouseEvent e){}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
 
 	public static void main(String[]args){
 		new DDA();
